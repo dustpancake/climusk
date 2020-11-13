97f3831f91a183eb54773d462527e354c11d9022
dustpancake <fergusbkr@gmail.com>
2020-11-13 21:43:35 +0100

    skeleton effort component

---
+++ frontend/src/components/Effort.vue
@@ -0,0 +1,91 @@
+<template>
+
+  <Card>
+
+  <b-row>
+
+    <b-col>
+      <h3 class="clinet-title">
+        {{title}}
+      </h3>
+    </b-col>
+
+    <b-col align="right">
+      <small class="text-muted">Authored by </small><br>
+      {{author}}
+    </b-col>
+
+  </b-row>
+
+  {{description}}
+
+  <b-row align="center" justify="center">
+
+  <b-col>
+    <b-btn variant="outline">
+      <router-link :to="effort_uri">See More</router-link>
+    </b-btn>
+  </b-col>
+
+  <b-col>
+    <b-btn variant="outline">
+      <router-link :to="effort_uri">Join</router-link>
+    </b-btn>
+  </b-col>
+
+  </b-row>
+
+
+
+  </Card>
+
+</template>
+
+
+<script>
+import Card from "./Card.vue"
+
+export default {
+
+  components: {
+    Card
+  },
+
+  props: {
+    title: {
+      type: String
+    },
+    tasks: {
+      type: Object,
+    },
+    author: {
+      type: String,
+      default: "TestAuthor"
+    },
+    e_id: { // effort id
+      type: String,
+      default: "0"
+    },
+    description: {
+      type: String,
+      default: "sample description"
+    }
+  },
+
+  computed: {
+    effort_uri: function () {
+      return "/effort/" + this.e_id;
+    }
+  }
+
+}
+
+</script>
+
+<style>
+
+.clinet-title {
+  color: green;
+}
+
+</style>
