137fad6e8463e2b78781b26b222b151296f2b1a0
gelpkmar <marlon.gelpke@gmail.com>
2020-11-13 20:19:34 +0100

    Update category.py

--- backend/climusk/models/category.py
+++ backend/climusk/models/category.py
@@ -1,7 +1,30 @@
 from pydantic import BaseModel, Field
 from typing import List
 
-class CategorySchema(BaseModel):
+class Category(BaseModel):
 
-    category_name: str = Field(...)
-    efforts: List = []
+    category_name: str
+    efforts: List = [Effort]
+
+class Effort(BaseModel):
+    author: str
+    tasks: List = [Task]
+
+class Task(BaseModel):
+    author: str
+    descr: str
+    participants: List = [User]
+
+class User(BaseModel):
+    name: str
+    
+    """
+    @validator('email')
+    def name_must_contain_space(cls, v):
+        if ' ' not in v:
+            raise ValueError('must contain a space')
+        return v.title()
+    """
+
+
+
