9eb9e4043857c854ad9e1301d9cf90bd916423fa
gelpkmar <marlon.gelpke@gmail.com>
2020-11-13 23:57:05 +0100

    Added effort endpoint for CliNet FastAPI

--- backend/climusk/routes/effort_router.py
+++ backend/climusk/routes/effort_router.py
@@ -1,31 +1,19 @@
 from fastapi import APIRouter
 
 from climusk.database import database
-from climusk.models.schema import CategorySchema, Task
+from climusk.models.schema import Effort, User
 
 
 router = APIRouter()
 
-efforts_collection = database.get_collection("efforts")
+effort_collection = database.get_collection("efforts")
 
-async def efforts_collection():
-    
-    return efforts_collection.find_one()
+async def retrieve_effort(effort_id):
+  ret = await effort_collection.find_one({"effort_id":effort_id})
+  return Effort(**ret)
 
-
-class Effort(BaseModel):
-
-    author_id: str = Field(...)
-    tasks: List[Task] = []
-    participants: List[User] =
-
-@router.get("/{name}")
-async def get_category(name: str):
-    return await retrieve_categories({"name":name})
+@router.get("/{oid}")
+async def get_effort(oid: str):
+    return await retrieve_effort(oid)
 
 
-@router.post("/task")
-async def post_task(task: Task):
-    print(type(task))
-    print(task)
-    return {}
