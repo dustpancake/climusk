69d5fa9751dd505834dcc14df61730c461164def
dustpancake <fergusbkr@gmail.com>
2020-11-13 22:02:46 +0100

    task outline component

---
+++ frontend/src/components/Task.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-row class="text-center">
+    <b-col ><b>{{name}}</b></b-col>
+    <b-col cols="8" align="left">{{description}}</b-col>
+  </b-row>
+</template>
+
+
+<script>
+export default {
+
+
+  props: {
+    description: {
+      type: String
+    },
+    name: {
+      type: String
+    }
+  }
+}
+
+</script>
