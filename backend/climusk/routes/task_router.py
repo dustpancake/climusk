56c13fcd47cd535c726cafcc83b3ab0b64d75e02
gelpkmar <marlon.gelpke@gmail.com>
2020-11-14 00:03:48 +0100

    Added Task Endpoint

---
+++ backend/climusk/routes/task_router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+from climusk.database import database
+from climusk.models.schema import Effort, User
+
+
+router = APIRouter()
+
+effort_collection = database.get_collection("efforts")
+
+async def retrieve_effort(effort_id):
+  ret = await effort_collection.find_one({"effort_id":effort_id})
+  return Effort(**ret)
+
+@router.get("/{oid}")
+async def get_effort(oid: str):
+    return await retrieve_effort(oid)
+
+
