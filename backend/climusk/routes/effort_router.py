bc1a6d043d50d2fc9dfb3bb5ed12b1d4b92b7d58
gelpkmar <marlon.gelpke@gmail.com>
2020-11-14 02:06:04 +0100

    changes in effort_router

--- backend/climusk/routes/effort_router.py
+++ backend/climusk/routes/effort_router.py
@@ -7,6 +7,7 @@ from climusk.models.schema import Effort, Task
 router = APIRouter()
 
 effort_collection = database.get_collection("efforts")
+task_collection = database.get_collection("tasks")
 
 async def retrieve_effort(effort_id):
     ret = await effort_collection.find_one({"effort_id":effort_id})
@@ -17,26 +18,23 @@ async def retrieve_effort(effort_id):
 async def get_effort(oid: str):
     return await retrieve_effort(oid)
 
-@router.post("/")
-async def insert_task(task: Task):
-  ret = await task_collection.insert_one(task.dict(exclude_unset=True))
-  ##print(ret.inserted_id)
-  return {"id": str(ret.inserted_id)}
-
 async def add_task_to_effort(task, effort):
+    ret = await task_collection.insert_one(task.dict(exclude_unset=True))
 
     cat = await effort_collection.find_one(
         {"effort_name": effort}
     ) 
     if not cat:
         return JSONResponse(status_code=400, content={"message": "bad category"})
+    
+    ret = await task_collection.insert_one(task.dict(exclude_unset=True))
 
     updated_tasks = cat.get("tasks", [])
     updated_tasks.append(task.dict(exclude_unset=True))
     resp = await effort_collection.update_one(
         {"effort_name": effort}, {"$set": {"tasks": updated_tasks}}
     )
-    return {"modified": resp.modified_count, "message": "ok"}
+    return {"id": str(ret.inserted_id), "modified": resp.modified_count, "message": "ok"}
 
 # add effort to category
 @router.post("/{effort}")
