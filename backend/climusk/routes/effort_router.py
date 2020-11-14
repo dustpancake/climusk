80e84a13fa0de1fb3af89b01e9b08dd0877ebbb5
gelpkmar <marlon.gelpke@gmail.com>
2020-11-14 01:42:08 +0100

    Edi effort_router

--- backend/climusk/routes/effort_router.py
+++ backend/climusk/routes/effort_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from climusk.database import database
-from climusk.models.schema import Effort
+from climusk.models.schema import Effort, Task
 
 
 router = APIRouter()
@@ -16,3 +16,29 @@ async def retrieve_effort(effort_id):
 @router.get("/{oid}")
 async def get_effort(oid: str):
     return await retrieve_effort(oid)
+
+@router.post("/")
+async def insert_task(task: Task):
+  ret = await task_collection.insert_one(task.dict(exclude_unset=True))
+  ##print(ret.inserted_id)
+  return {"id": str(ret.inserted_id)}
+
+async def add_task_to_effort(task, effort):
+
+    cat = await effort_collection.find_one(
+        {"effort_name": effort}
+    ) 
+    if not cat:
+        return JSONResponse(status_code=400, content={"message": "bad category"})
+
+    updated_tasks = cat.get("tasks", [])
+    updated_tasks.append(task.dict(exclude_unset=True))
+    resp = await effort_collection.update_one(
+        {"effort_name": effort}, {"$set": {"tasks": updated_tasks}}
+    )
+    return {"modified": resp.modified_count, "message": "ok"}
+
+# add effort to category
+@router.post("/{effort}")
+async def add_task(effort: str, task: Task):
+    return await add_task_to_effort(task, effort)
--- backend/climusk/routes/task_router.py
+++ backend/climusk/routes/task_router.py
@@ -1,29 +1,16 @@
 from fastapi import APIRouter
 
 from climusk.database import database
-from climusk.models.schema import Task, User, Effort
+from climusk.models.schema import Task
 
 
 router = APIRouter()
 
 task_collection = database.get_collection("tasks")
-effort_collection = database.get_collection("efforts")
-
 
 async def retrieve_task(task_id):
   ret = await task_collection.find_one({"task_id":task_id})
   return Task(**ret)
-
-@router.post("/")
-async def insert_task(task: Task):
-  ret = await task_collection.insert_one(task.dict(exclude_unset=True))
-  ##print(ret.inserted_id)
-  return {"id": str(ret.inserted_id)}
-
-@router.post("/{effort_oid}")
-async def add_task_to_effort(effort_oid: str, task: Task):
-  ret = await effort_collection.insert_one(task.dict(exclude_unset=True))
-
 
 @router.get("/{oid}")
 async def get_task(oid: str):
